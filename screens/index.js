a01a1bdb7fa2d8473fafbc85d08c76bff7869fd1
Jaem <52319781+Jaem123@users.noreply.github.com>
2021-03-08 19:48:09 +0100

    Hamburger menu

---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Screen from './Screen';
+
+export const SignUserScreen = ({navigation}) => <Screen navigation={navigation} name="SignUser" />
+export const UserListScreen = ({navigation}) => <Screen navigation={navigation} name="UserList" />
